384c6c96405133115014b74caa2ce0975ac65d12
liztaliah <liz.t.hardee@gmail.com>
2025-06-11 00:22:45 -0700

    adjusted login page to match the site style

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,65 +1,66 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Button from "./Button";
 
 function LoginForm() {
-    const [form, setForm] = useState({username: "", password: ""});
-    const [error, setError] = useState(null);
-    const navigate = useNavigate();
+  const [form, setForm] = useState({ username: "", password: "" });
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-    // Update the username/password form upon text entry
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
+  // Update the username/password form upon text entry
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    // Submit the username/password and await the response from the API
-    const handleSubmit = async (e) => {
-        e.preventDefault() // Prevents the page from reloading
-        setError(null);    // Clear old errors
+  // Submit the username/password and await the response from the API
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevents the page from reloading
+    setError(null); // Clear old errors
 
-        try {
-            const response  = await axios.post("/api/login", form);
+    try {
+      const response = await axios.post("/api/login", form);
 
-            // Save token to localStorage
-            localStorage.setItem("token", response.data.token);
+      // Save token to localStorage
+      localStorage.setItem("token", response.data.token);
 
-            // Redirect to the user list -- Change This to HomePage
-            navigate("/home");
-        } catch (err) {
-            if (err.response && err.response.data) {
-                setError(err.response.data.error);
-            } else {
-                setError("An unknown error occured.")
-            }
-        }
-    };
+      // Redirect to the user list -- Change This to HomePage
+      navigate("/home");
+    } catch (err) {
+      if (err.response && err.response.data) {
+        setError(err.response.data.error);
+      } else {
+        setError("An unknown error occured.");
+      }
+    }
+  };
 
-    return (
-        <div style={{ maxWidth: 400, magin: "0 auto" }}>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    name="username"
-                    value={form.username}
-                    onChange={handleChange}
-                    placeholder="Username"
-                    required
-                />
-                <br />
-                <input 
-                    type="password" 
-                    name="password"
-                    value={form.password}
-                    onChange={handleChange}
-                    placeholder="Password"
-                    required
-                />
-                <br />
-                <button type="submit">Log In</button>
-            </form>
-            {error && <p style={{ color: "red"}}> {error}</p>}
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4">
+      <h1 className="text-4xl font-semibold mb-4 tracking-tight">_Login_</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <input
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+          placeholder="Username"
+          required
+          className="px-3 py-2 rounded"
+        />
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          placeholder="Password"
+          required
+          className="mb-4 px-3 py-2 rounded"
+        />
+        <Button type="submit">Log In</Button>
+      </form>
+      {error && <p style={{ color: "red" }}> {error}</p>}
+    </div>
+  );
 }
 
 export default LoginForm;
