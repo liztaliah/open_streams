e619a54b9cac7ac9f2e0e115f752e69fc532392f
liztaliah <liz.t.hardee@gmail.com>
2025-06-13 16:47:14 -0700

    just formatted a couple things

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export async function submitAuthForm(endpoint, form, setError, setShowError, onSuccess) {
+export async function submitAuthForm(
+  endpoint,
+  form,
+  setError,
+  setShowError,
+  onSuccess
+) {
   setError(null);
   try {
     await axios.post(endpoint, form);
