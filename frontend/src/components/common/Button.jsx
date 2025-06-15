7017c7dedd2a25db786daa63069fd6444a54d0f4
liztaliah <liz.t.hardee@gmail.com>
2025-06-14 20:58:49 -0700

    override ugly blue border from browser for buttons

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -1,7 +1,19 @@
 export default function Button({ children, classname = "", ...props }) {
   return (
     <button
-      className={`w-24 px-4 py-2 bg-neutral-700 rounded-full hover:bg-neutral-600 transition ${classname}`}
+      className={`
+        focus:outline-none
+        focus:ring-2
+        focus:ring-white
+        w-24 
+        px-4 
+        py-2 
+        bg-neutral-700 
+        rounded-full 
+        hover:bg-neutral-600 
+        transition 
+        duration-150
+        ${classname}`}
       {...props}
     >
       {children}
