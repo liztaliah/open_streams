353e736ca0d8fa792a68c93b82ad613dbe6d4090
liztaliah <liz.t.hardee@gmail.com>
2025-06-16 10:14:59 -0700

    updated createroom form to use username as room name

--- frontend/src/components/pages/CreateRoomForm.jsx
+++ frontend/src/components/pages/CreateRoomForm.jsx
@@ -1,29 +1,22 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../common/Button";
 import ErrorMessage from "../common/ErrorMessage";
 import FormContainer from "../layout/FormContainer";
 import { submitForm } from "../../utils/submitForm";
-import Input from "../common/Input";
+import { UserContext } from "../../context/UserContext";
 
 export default function CreateRoomForm() {
-  const [name, setName] = useState("");
   const [error, setError] = useState(null);
   const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
+  const { username } = useContext(UserContext); // Get username from context
 
-  // Set input on form change
-  const handleChange = (e) => {
-    if (error) setShowError(false);
-    setName(e.target.value);
-  };
-
-  // Submit room creation form
   const handleSubmit = async (e) => {
     e.preventDefault();
     submitForm(
       "/api/rooms",
-      { name },
+      { name: username }, // Use username as room name
       setError,
       setShowError,
       (response) => {
@@ -34,15 +27,7 @@ export default function CreateRoomForm() {
   };
 
   return (
-    <div
-      className="
-        flex 
-        flex-col 
-        items-center 
-        justify-center
-        min-h-screen 
-        px-4"
-    >
+    <div className="flex flex-col items-center justify-center min-h-screen px-4">
       <h1 className="text-4xl font-semibold mb-4 tracking-tight">
         _CreateRoom_
       </h1>
@@ -58,15 +43,7 @@ export default function CreateRoomForm() {
           onSubmit={handleSubmit}
           className="flex flex-col items-center w-full max-w-xs"
         >
-          <Input
-            type="text"
-            value={name}
-            onChange={handleChange}
-            placeholder="Room Name"
-            required
-            className="text-center rounded-lg mt-2 mb-8"
-          />
-          <Button type="submit">Create</Button>
+          <Button type="submit">Host</Button>
         </form>
       </FormContainer>
     </div>
