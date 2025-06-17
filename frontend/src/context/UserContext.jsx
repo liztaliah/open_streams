9dcfb25d5e481138103b563ce73acb9506bab7df
liztaliah <liz.t.hardee@gmail.com>
2025-06-16 22:24:14 -0700

    changed UserContext component to save username in local stoare to persist upon reload

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,10 +1,22 @@
 // Set up a context to access usernames from anywhere in the app
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
-  const [username, setUsername] = useState(null);
+  // Initilaze username from localStorage
+  const [username, setUsername] = useState(() =>
+    localStorage.getItem("username")
+  );
+
+  // Sync local storage with state
+  useEffect(() => {
+    if (username) {
+      localStorage.setItem("username", username);
+    } else {
+      localStorage.removeItem("username");
+    }
+  }, [username]);
 
   return (
     <UserContext.Provider value={{ username, setUsername }}>
